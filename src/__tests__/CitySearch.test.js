6d09226e20408eed0a5c82f7d47abab8a89c65d4
Kirk Meyer <kirkmeyer03@gmail.com>
2024-01-24 13:40:39 -0800

    Integrated infoAlert

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -9,7 +9,11 @@ import { extractLocations, getEvents } from '../api';
 describe('<CitySearch /> component', () => {
   let CitySearchComponent;
   beforeEach(() => {
-    CitySearchComponent = render(<CitySearch allLocations={[]} />);
+    CitySearchComponent = render(<CitySearch 
+      allLocations={[]}
+      setCurrentCity={() => {}} 
+      setInfoAlert={() => {}}
+    />);
   })
 
   test('renders text input', () => {
@@ -36,7 +40,7 @@ describe('<CitySearch /> component', () => {
     const user = userEvent.setup();
     const allEvents = await getEvents();
     const allLocations = extractLocations(allEvents);
-    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} setInfoAlert={() => {}} />);
 
     //user types Berlin into search bar
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
@@ -58,7 +62,7 @@ describe('<CitySearch /> component', () => {
     const user = userEvent.setup();
     const allEvents = await getEvents();
     const allLocations = extractLocations(allEvents);
-    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} setInfoAlert={() => {}} />);
 
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
     await user.type(cityTextBox, 'Paris, France');
@@ -73,7 +77,8 @@ describe('<CitySearch /> component', () => {
     const allLocations = extractLocations(allEvents);
     CitySearchComponent.rerender(<CitySearch
       allLocations={allLocations}
-      setCurrentCity={() => { }} 
+      setCurrentCity={() => {}}
+      setInfoAlert={() => {}} 
     />);
 
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
