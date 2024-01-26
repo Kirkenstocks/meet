7e35324c07bdbdb89b2834973f33b4441ca45cb6
Kirk Meyer <kirkmeyer03@gmail.com>
2024-01-26 15:01:53 -0800

    Added warning alert with color

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -48,4 +48,12 @@ class ErrorAlert extends Alert {
   }
 };
 
-export { InfoAlert, ErrorAlert };
+class WarningAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'rgb(255, 141, 17)';
+    this.bgColor = 'rgb(252, 193, 129)';
+  }
+};
+
+export { InfoAlert, ErrorAlert, WarningAlert };
